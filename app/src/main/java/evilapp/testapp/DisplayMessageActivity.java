00bd7ce69686a4bc4d8b0eb4c8110e24e67857ff
evilapp <evilandroidapp@gmail.com>
2014-08-19 16:56:08 -0700

    Finished tutorial for first app

--- app/src/main/java/evilapp/testapp/DisplayMessageActivity.java
+++ app/src/main/java/evilapp/testapp/DisplayMessageActivity.java
@@ -1,29 +1,43 @@
 package evilapp.testapp;
 
 import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
 import android.content.Intent;
-import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class MainActivity extends Activity {
 
-    public final static String EXTRA_MESSAGE = "evilapp.testapp.MESSAGE";
+public class DisplayMessageActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.display_message, menu);
         return true;
     }
 
@@ -37,15 +51,5 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        // Do something in response to button
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
     }
 }
